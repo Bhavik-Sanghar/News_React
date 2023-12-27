8ab609520f39b0c255d82081e5f72743b1b93fbc
Bhavik-Sanghar <130123723+Bhavik-Sanghar@users.noreply.github.com>
2023-12-27 16:55:31 +0530

    Update

--- src/component/AllNews.js
+++ src/component/AllNews.js
@@ -1,52 +1,72 @@
-import React, { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
-import "./index.css";
-
-
-function AllNews() {
+function NewsApp() {
   const [newsData, setNewsData] = useState([]);
 
-  const fetchNewsData = async () => {
-    try {
-      const response = await fetch("https://inshorts.me/news/all?offset=0&limit=21");
-      const data = await response.json();
-      setNewsData(data.data.articles);
-      console.log(data);
-    } catch (error) {
-      console.error("Error fetching news data:", error);
-    }
-  };
-
   useEffect(() => {
-    fetchNewsData();
-    const interval = setInterval(fetchNewsData, 500000); // Fetch news every 5 minutes (300000 milliseconds)
-    return () => clearInterval(interval);
+    const fetchData = async () => {
+      try {
+        const apiKey = "0a7dba7951b849b2b5494b80691e1e99";
+        const response = await fetch(
+          `https://newsapi.org/v2/everything?q=technology&apiKey=${apiKey}`
+        );
+        const data = await response.json();
+        setNewsData(data.articles);
+      } catch (error) {
+        console.error("Error fetching news data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  
+  const limitedNewsData = newsData.slice(0, 15);
+
   return (
-    <div className="container mt-4">
-      <h2 className="NewsHead">All News</h2>
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        {newsData.map((newsItem) => (
-          <div className="col" key={newsItem.hashId}>
-            <Card className="h-100 card-hover-effect" id="Card">
-              <Card.Img variant="top" src={newsItem.imageUrl} />
-              <Card.Body className="card-body">
+    <Container className="mt-4">
+      <h2 className="NewsHead">Latest Technology News</h2>
+      <Row xs={1} md={3} className="g-4">
+        {limitedNewsData.map((newsItem, index) => (
+          <Col key={index}>
+            <Card
+              className="card-hover-effect"
+              id="Card"
+              style={{
+                background: 'rgba( 255, 255, 255, 0.25 )',
+                boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                backdropFilter: 'blur( 4px )',
+                WebkitBackdropFilter: 'blur( 4px )',
+                borderRadius: '30px',
+                border: '1px solid rgba( 255, 255, 255, 0.18 )',
+                height: '100%',
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src={newsItem.urlToImage}
+                className="card-img"
+                style={{
+                  objectFit: 'cover',
+                  height: '200px',
+                }}
+              />
+              <Card.Body>
                 <Card.Title>{newsItem.title}</Card.Title>
-                <Card.Text>{newsItem.content}</Card.Text>
+                <Card.Text>{newsItem.description}</Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">{newsItem.sourceName}</small>
+                <small className="text-muted">{newsItem.source.name}</small>
               </Card.Footer>
             </Card>
-          </div>
+          </Col>
         ))}
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 }
 
-export default AllNews;
+export default NewsApp;
--- src/component/EntrNews.js
+++ src/component/EntrNews.js
@@ -1,49 +1,78 @@
-import React, { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
-import "./index.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Button } from "react-bootstrap"; // Import Button from react-bootstrap
 
+import Container from "react-bootstrap/Container";
 
-function EntrNews() {
+function NewsApp() {
   const [newsData, setNewsData] = useState([]);
 
-  const fetchNewsData = async () => {
-    try {
-      const response = await fetch("https://inshorts.me/news/topics/entertainment");
-      const data = await response.json();
-      setNewsData(data.data.articles);
-    } catch (error) {
-      console.error("Error fetching news data:", error);
-    }
-  };
-
   useEffect(() => {
-    fetchNewsData();
-    const interval = setInterval(fetchNewsData, 300000); // Fetch news every 5 minutes (300000 milliseconds)
-    return () => clearInterval(interval);
+    const fetchData = async () => {
+      try {
+        const apiKey = "0a7dba7951b849b2b5494b80691e1e99";
+        const response = await fetch(
+          `https://newsapi.org/v2/everything?q=Health&apiKey=${apiKey}`
+        );
+        const data = await response.json();
+        console.log(data)
+        setNewsData(data.articles);
+      } catch (error) {
+        console.error("Error fetching news data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  const limitedNewsData = newsData.slice(0, 15);
+
   return (
-    <div className="container mt-4">
-      <h2 className="NewsHead">Entertainment News</h2>
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        {newsData.map((newsItem) => (
-          <div className="col" key={newsItem.hashId}>
-            <Card className="h-100 card-hover-effect ">
-              <Card.Img variant="top" src={newsItem.imageUrl} />
+    <Container className="mt-4" >
+      <h2 className="NewsHead">Health News</h2>
+      <Row xs={1} md={3} className="g-4">
+        {limitedNewsData.map((newsItem, index) => (
+          <Col key={index}>
+            <Card
+              className="card-hover-effect"
+              id="Card"
+              style={{
+                background: 'rgba( 255, 255, 255, 0.25 )',
+                boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                backdropFilter: 'blur( 4px )',
+                WebkitBackdropFilter: 'blur( 4px )',
+                borderRadius: '30px',
+                border: '1px solid rgba( 255, 255, 255, 0.18 )',
+                height: '100%',
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src={newsItem.urlToImage}
+                className="card-img"
+                style={{
+                  objectFit: 'cover',
+                  height: '200px',
+                }}
+              />
               <Card.Body>
                 <Card.Title>{newsItem.title}</Card.Title>
-                <Card.Text>{newsItem.content}</Card.Text>
+                <Card.Text>{newsItem.description}</Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">{newsItem.sourceName}</small>
+                <Button href={newsItem.url} target="_blank">
+                  Read More
+                </Button>
+                <small className="text-muted">{newsItem.source.name}</small>
               </Card.Footer>
             </Card>
-          </div>
+          </Col>
         ))}
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 }
 
-export default EntrNews;
+export default NewsApp;
